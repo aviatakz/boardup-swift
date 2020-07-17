9c1102cd58ef6734fcab270d2ea352bdd8f832f4
Adilzhan <Adil02012000@gmail.com>
2020-07-17 17:49:40 +0600

    Diagram View 1.0

---
+++ 360/View/SubDiagramView.swift
@@ -0,0 +1,16 @@
+//
+//  SubDiagramView.swift
+//  360
+//
+//  Created by Adilzhan Akhayev on 7/17/20.
+//  Copyright © 2020 Adilzhan. All rights reserved.
+//
+
+import UIKit
+
+class SubDiagramView: UIView {
+    
+    
+
+
+}
