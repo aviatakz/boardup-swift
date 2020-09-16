ad4d9bf8b80813cf1a82893734f88ec28d453435
Adilzhan <Adil02012000@gmail.com>
2020-09-16 18:05:36 +0600

    create LocalData and Objects model

---
+++ 360/Model/LocalData.swift
@@ -0,0 +1,15 @@
+//
+//  LocalData.swift
+//  360
+//
+//  Created by Adilzhan Akhayev on 9/16/20.
+//  Copyright © 2020 Adilzhan. All rights reserved.
+//
+
+import Foundation
+
+struct LocalData {
+    static var userId: Int = 0
+    static var token: String = ""
+    static var surveyId: Int = 41
+}
